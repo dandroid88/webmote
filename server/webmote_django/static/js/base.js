cc9c6f6bcae32a25a21c41da8d4952827f97cb67
dandroid88 <dmritard96@gmail.com>
2012-12-14 14:46:56 -0600

    added a side menu

--- server/webmote_django/static/js/base.js
+++ server/webmote_django/static/js/base.js
@@ -47,3 +47,48 @@ function runAction(url) {
         url : url,
     });
 }
+
+$(function(){
+	var menuStatus;
+	
+	$("a#showMenu").click(function(){
+		if(menuStatus != true){				
+		$(".ui-page-active").animate({
+			marginLeft: "165px",
+		  }, 300, function(){menuStatus = true});
+		  return false;
+		  } else {
+			$(".ui-page-active").animate({
+			marginLeft: "0px",
+		  }, 150, function(){menuStatus = false});
+			return false;
+		  }
+	});
+
+	$('.ui-page').live("swipeleft", function(){
+		if (menuStatus){	
+		$(".ui-page-active").animate({
+			marginLeft: "0px",
+		  }, 150, function(){menuStatus = false});
+		  }
+	});
+	
+	$('.ui-page').live("swiperight", function(){
+		if (!menuStatus){	
+		$(".ui-page-active").animate({
+			marginLeft: "165px",
+		  }, 150, function(){menuStatus = true});
+		  }
+	});
+	
+	$("#menu li a").click(function(){
+		var p = $(this).parent();
+		if($(p).hasClass('active')){
+			$("#menu li").removeClass('active');
+		} else {
+			$("#menu li").removeClass('active');
+			$(p).addClass('active');
+		}
+	});
+		
+});	
